19536a6e0552e69b37dd864b5dd87b21d273b8c5
VasanthaKumar Annadurai <vasanthakumar.annadurai@freshworks.com>
2022-10-30 08:17:37 +0530

    Initial Commit - With Empty Project.

---
+++ Podfile
@@ -0,0 +1,5 @@
+platform :ios, '13.0'
+
+target 'iOS-AppArchitecture' do
+  use_frameworks!
+end
---
+++ iOS-AppArchitecture/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iOS-AppArchitecture
+//
+//  Created by Vasanthakumar Annadurai on 30/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
